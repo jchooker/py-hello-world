1175ae5342ee58a5991e746f07ee10d343a4877e
Joseph Hooker <jcolehooker@gmail.com>
2021-09-05 16:17:04 -0500

    Did a few more bonus portions

--- hello-world.py
+++ hello-world.py
@@ -9,8 +9,17 @@ print("Hello,", num, "!")
 print("Hello, " + str(num) + "!") #resolved issue with concatenation
 
 food1 = "tuna mac"
-food2 = "chick alfredo"
+food2 = "veggie pizza"
 actual_age = 36
 print("I love to eat {} and {}, despite being {}".format(food1, food2, actual_age)) #4a
 
 print(f"I love to eat {food1} and {food2}, despite being {actual_age} years of age.") #4b
+
+str1 = "an example of a string to do various operations with" #last bonus section
+
+print(str1.split())
+
+prep1 = str1.count("of")
+prep2 = str1.count("with")
+
+print(f"There are {prep1+prep2} prepositions in this string")
