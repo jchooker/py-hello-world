e86466608a55b5026650f895c7f178210b26aaaf
Joseph Hooker <jcolehooker@gmail.com>
2021-09-05 15:46:51 -0500

    Actually completed the basic 'Hello World' assignments, in addition to the bonus from earlier.

--- hello-world.py
+++ hello-world.py
@@ -5,5 +5,12 @@
 # print("My name is", name)
 
 num = 26
-print("Hello,",num, "!")
-print("Hello," + num + "!")
+print("Hello,", num, "!")
+print("Hello, " + str(num) + "!") #resolved issue with concatenation
+
+food1 = "tuna mac"
+food2 = "chick alfredo"
+actual_age = 36
+print("I love to eat {} and {}, despite being {}".format(food1, food2, actual_age)) #4a
+
+print(f"I love to eat {food1} and {food2}, despite being {actual_age} years of age.") #4b
